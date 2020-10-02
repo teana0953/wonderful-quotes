6a40194a07e0af83fd1dd0b981df72db629d447d
Tina <tina@catchyourhealth.com.tw>
2020-10-03 00:08:23 +0800

    Update, add QuoteGrid to show Quotes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
-    <div class="container"></div>
+    <div class="container">
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
 </template>
 
 <script>
-export default {};
+import QuoteGrid from './components/QuoteGrid.vue';
+
+export default {
+    data: function () {
+        return {
+            quotes: ['Just a Quote to see something'],
+            maxQuotes: 10,
+        };
+    },
+    components: {
+        appQuoteGrid: QuoteGrid,
+    },
+};
 </script>
 
 <style></style>
