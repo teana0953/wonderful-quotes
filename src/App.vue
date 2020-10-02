b923f86357d91ef623d85b4c2d0eb27bae1d4bd1
Tina <tina@catchyourhealth.com.tw>
2020-10-03 00:13:19 +0800

    Update, add progress bar feature and alert message

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
         <app-new-quote @quoteAdded="newQuote"></app-new-quote>
         <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
         <div class="row">
@@ -13,6 +14,7 @@
 <script>
 import QuoteGrid from './components/QuoteGrid.vue';
 import NewQuote from './components/NewQuote.vue';
+import Header from './components/Header.vue';
 
 export default {
     data: function () {
@@ -23,6 +25,9 @@ export default {
     },
     methods: {
         newQuote(quote) {
+            if (this.quotes.length >= this.maxQuotes) {
+                return alert('Please delete Quotes first!');
+            }
             this.quotes.push(quote);
         },
         deleteQuote(index) {
@@ -32,6 +37,7 @@ export default {
     components: {
         appQuoteGrid: QuoteGrid,
         appNewQuote: NewQuote,
+        appHeader: Header,
     },
 };
 </script>
